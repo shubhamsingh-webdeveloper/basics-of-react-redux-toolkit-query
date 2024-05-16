868812b4601ce4ff8c18585c9e0f08256e7b6211
Shubham Singh <shubham@svahatech.com>
2024-05-16 12:07:05 +0530

    Loader Added

--- src/components/FetchTodos.jsx
+++ src/components/FetchTodos.jsx
@@ -4,6 +4,7 @@ import AddTodo from "./AddTodo";
 import { useFetchTodosQuery } from "../features/actions/todosApi";
 import { useDispatch } from "react-redux";
 import { saveTodosList } from "../features/slices/todosSlice";
+import Loader from "./utils/Loader";
 
 const FetchTodos = () => {
   const dispatch = useDispatch();
@@ -13,65 +14,136 @@ const FetchTodos = () => {
     if (data) dispatch(saveTodosList(data));
   }, [data, dispatch]);
 
-  if (isLoading) return <div>Loading...</div>;
+  // if (isLoading) return <div>Loading...</div>;
+  // if (isLoading)
+  //   return (
+  //     <div>
+  //       <Loader />
+  //     </div>
+  //   );
   if (error) return <div>Error: {error.message}</div>;
 
   return (
     <div>
       <AddTodo />
-      <table
-        style={{
-          width: "100%",
-          borderCollapse: "collapse",
-          margin: "0 auto",
-        }}
-      >
-        <thead>
-          <tr>
-            <th
-              style={{
-                padding: "8px",
-                backgroundColor: "#f2f2f2",
-                border: "1px solid #ddd",
-              }}
-            >
-              S.No.
-            </th>
-            <th
-              style={{
-                padding: "8px",
-                backgroundColor: "#f2f2f2",
-                border: "1px solid #ddd",
-              }}
-            >
-              Task Id
-            </th>
-            <th
-              style={{
-                padding: "8px",
-                backgroundColor: "#f2f2f2",
-                border: "1px solid #ddd",
-              }}
-            >
-              Task
-            </th>
-            <th
-              style={{
-                padding: "8px",
-                backgroundColor: "#f2f2f2",
-                border: "1px solid #ddd",
-              }}
-            >
-              Actions
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((todo, i) => (
-            <IndividualTodo key={todo.id} todo={todo} index={i} />
-          ))}
-        </tbody>
-      </table>
+
+      {isLoading ? (
+        <>
+          <table
+            style={{
+              width: "100%",
+              borderCollapse: "collapse",
+              margin: "0 auto",
+            }}
+          >
+            <thead>
+              <tr>
+                <th
+                  style={{
+                    padding: "8px",
+                    backgroundColor: "#f2f2f2",
+                    border: "1px solid #ddd",
+                  }}
+                >
+                  S.No.
+                </th>
+                <th
+                  style={{
+                    padding: "8px",
+                    backgroundColor: "#f2f2f2",
+                    border: "1px solid #ddd",
+                  }}
+                >
+                  Task Id
+                </th>
+                <th
+                  style={{
+                    padding: "8px",
+                    backgroundColor: "#f2f2f2",
+                    border: "1px solid #ddd",
+                  }}
+                >
+                  Task
+                </th>
+                <th
+                  style={{
+                    padding: "8px",
+                    backgroundColor: "#f2f2f2",
+                    border: "1px solid #ddd",
+                  }}
+                >
+                  Actions
+                </th>
+              </tr>
+            </thead>
+          </table>
+          <div
+            style={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Loader />
+          </div>
+          <p style={{ textAlign: "center" }}>Loading... Please wait</p>
+        </>
+      ) : (
+        <table
+          style={{
+            width: "100%",
+            borderCollapse: "collapse",
+            margin: "0 auto",
+          }}
+        >
+          <thead>
+            <tr>
+              <th
+                style={{
+                  padding: "8px",
+                  backgroundColor: "#f2f2f2",
+                  border: "1px solid #ddd",
+                }}
+              >
+                S.No.
+              </th>
+              <th
+                style={{
+                  padding: "8px",
+                  backgroundColor: "#f2f2f2",
+                  border: "1px solid #ddd",
+                }}
+              >
+                Task Id
+              </th>
+              <th
+                style={{
+                  padding: "8px",
+                  backgroundColor: "#f2f2f2",
+                  border: "1px solid #ddd",
+                }}
+              >
+                Task
+              </th>
+              <th
+                style={{
+                  padding: "8px",
+                  backgroundColor: "#f2f2f2",
+                  border: "1px solid #ddd",
+                }}
+              >
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((todo, i) => (
+              <IndividualTodo key={todo.id} todo={todo} index={i} />
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
